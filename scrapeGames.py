eabf2b67713135589ca36c0819daa2d3535cb773
William Goins <de777urest@gmail.com>
2017-04-13 22:31:09 -0500

    Wrote out rest of scrapeGames.py

--- scrapeGames.py
+++ scrapeGames.py
@@ -27,13 +27,44 @@ for index, row in teams.iterrows():
         #print columns;
         try:
             _home = True if columns[1].li.text == 'vs' else False
-            print _home
-            #_other_team = columns[1].find_all('a')[1].text
+            #print _home
+            _other_team = columns[1].find_all('a')[1].text
             #print _other_team
-            #_score = columns[2].a.text.split(' ')[0].split('-')
+            _score = columns[2].a.text.split(' ')[0].split('-')
             #print _score
-            #_won = True if columns[2].span.text == 'W' else False
+            _won = True if columns[2].span.text == 'W' else False
             #print won
+            
+            match_id.append(columns[2].a['href'].split('?id=')[1])
+            #print match_id
+            home_team.append(_team if _home else _other_team)
+            #print home_team
+            visit_team.append(_team if not _home else _other_team)
+            print visit_team
+            d = datetime.strptime(columns[0].text, '%a, %b %d')
+            print d
+            dates.append(date(year, d.month, d.day))
+            print dates
+            
+            if _home:
+                if _won:
+                    home_team_score.append(_score[0])
+                    visit_team_score.append(_score[1])
+                else:
+                    home_team_score.append(_score[1])
+                    visit_team_score.append(_score[0])
+            else:
+                if _won:
+                    home_team_score.append(_score[1])
+                    visit_team_score.append(_score[0])
+                else:
+                    home_team_score.append(_score[0])
+                    visit_team_score.append(_score[1])
         except Exception as e:
             pass # Not all columns row are a match, it's OK
             # print(e)
+
+dic = {'id': match_id, 'date': dates, 'home_team': home_team, 'visit_team': visit_team, 'home_team_score': home_team_score, 'visit_team_score': visit_team_score}
+
+games = pd.DataFrame(dic).drop_duplicates(cols='id').set_index('id')
+print(games)
